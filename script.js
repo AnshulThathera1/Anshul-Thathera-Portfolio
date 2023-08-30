35b5fa1519b22f92f4c546a78bf4c8e3851c58de
AnshulThathera1 <116070067+AnshulThathera1@users.noreply.github.com>
2023-08-30 14:25:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings:[" ","Web Developer","Web Designer","Freelancer"],
+    strings:["","Web Developer","Web Designer","Freelancer"],
     typeSpeed:250,
     BackSpeed:100,
     loop:true
